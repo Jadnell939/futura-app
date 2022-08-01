bab34c53dfa0615a95e8111c31bd930c1387bb58
Jadnell939 <110359880+Jadnell939@users.noreply.github.com>
2022-08-01 07:50:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ function go(){
 var usuario=document.form.email.value; 
 var password=document.form.text.value; 
 if (usuario=="marcelo" && password=="12345") { 
-window.location="TU_PAGINA_WEB.HTML"; 
+// window.location="TU_PAGINA_WEB.HTML";
+console.log("correcto"); 
 } 
 if (usuario=="marcelo@futura.com" && password=="12345") { 
-window.location="TU_PAGINA_WEB.HTML"; 
+// window.location="TU_PAGINA_WEB.HTML"; 
+console.log("correcto");
 } 
 if (usuario=="kdkdk" && password=="fkdk") { 
-window.location="errorpopup.html"; 
+//window.location="errorpopup.html"; 
+console.log("correcto");
 } else{
     alert("Verificar: Correo electrónico o contraseña incorrectos");
 }

cc2cdcc42730462fa3d7ec0b64e42a11380e4b95
Jadnell939 <110359880+Jadnell939@users.noreply.github.com>
2022-08-01 07:40:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function go(){
 
 var usuario=document.form.email.value; 
 var password=document.form.text.value; 
-if (usuario=="USUARIO1" && password=="CONTRASEÑA1") { 
+if (usuario=="marcelo" && password=="12345") { 
 window.location="TU_PAGINA_WEB.HTML"; 
 } 
 if (usuario=="USUARIO2" && password=="CONTRASEÑA2") { 
